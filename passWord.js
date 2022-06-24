532bfbf080c91d4216ee4146e3fb540fa3bd7572
Naldo de Andrade <106287645+francinaldodeandrade@users.noreply.github.com>
2022-06-24 12:46:04 -0300

    detalhes do projeto

---
+++ passWord.js
@@ -0,0 +1,11 @@
+var botão = document.querySelector('input .submit')
+
+
+    
+botao.addEventListener("click",() => {
+  
+    botão.innerHTML = ('index.html');
+  
+  })
+  
+  document.innerHTML(botão);
